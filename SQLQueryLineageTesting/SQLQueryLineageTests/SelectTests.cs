dacc81c59799aae757c465fb692ba492d2ecc2f1
Simen Nielsen <simen.nielsen96@gmail.com>
2023-05-12 22:43:11 +0200

    Select tests

---
+++ SQLQueryLineageTesting/SQLQueryLineageTests/SelectTests.cs
@@ -0,0 +1,21 @@
+﻿using SQLQueryLineage;
+
+namespace SQLQueryLineageTesting
+{
+    [TestClass]
+    public class SelectTests
+    {
+        public SelectTests() { }
+        [TestMethod]
+        public void AssertSimpleSelectColumnName()
+        {
+            string query = "SELECT " +
+                "col1 " +
+                "FROM Students";
+            SQLQueryLineageVisitor result = SQLQueryLineageProgram.GetStatementTargets(query);
+            string expected = "col1";
+            string compare = result.ProcedureEvents[0].GetColumns()[0].name;
+            Assert.AreEqual(expected, compare);
+        }
+    }
+}
